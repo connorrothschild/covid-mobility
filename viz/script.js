b6477c197e67f71573d6aa90809736458f62fe17
Connor Rothschild <connorrothschild@gmail.com>
2020-04-10 20:29:11 -0500

    finish slider version

--- viz/script.js
+++ viz/script.js
@@ -1,15 +1,6 @@
-var svg = d3
-	.select('svg')
-	.attr('class', 'my_chart')
-	.attr('height', 600)
-	.attr('width', 960)
-	// resize plot when window is resized (see below)
-	.call(responsivefy);
-
-var path = d3.geoPath();
-var format = d3.format('');
-var height = 600;
-var width = 960;
+var width = 960,
+	height = 500,
+	formatPercent = d3.format('.0%');
 
 var margin = {
 	top    : 40,
@@ -18,394 +9,168 @@ var margin = {
 	left   : 40
 };
 
-// thanks to https://brendansudol.com/writing/responsive-d3 for this function!
-function responsivefy(svg) {
-	// container will be the DOM element
-	// that the svg is appended to
-	// we then measure the container
-	// and find its aspect ratio
-	const container = d3.select(svg.node().parentNode),
-		width = parseInt(svg.style('width'), 10),
-		height = parseInt(svg.style('height'), 10),
-		aspect = width / height;
+var svg = d3
+	.select('#map')
+	.append('svg')
+	.attr('width', width + margin.left + margin.right)
+	.attr('height', height + margin.top + margin.bottom)
+	.append('g')
+	.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-	// set viewBox attribute to the initial size
-	// control scaling with preserveAspectRatio
-	// resize svg on inital page load
-	svg.attr('viewBox', `0 0 ${width} ${height}`).attr('preserveAspectRatio', 'xMinYMid').call(resize);
-
-	// add a listener so the chart will be resized
-	// when the window resizes
-	// multiple listeners for the same event type
-	// requires a namespace, i.e., 'click.foo'
-	// api docs: https://goo.gl/F3ZCFr
-	d3.select(window).on('resize.' + container.attr('id'), resize);
-
-	// this is the code that resizes the chart
-	// it will be called on load
-	// and in response to window resizes
-	// gets the width of the container
-	// and resizes the svg to fill it
-	// while maintaining a consistent aspect ratio
-	function resize() {
-		const w = parseInt(container.style('width'));
-		svg.attr('width', w);
-		svg.attr('height', Math.round(w / aspect));
-	}
-}
-
-function dateFunction(date) {
-	var formatTime = d3.timeFormat('%B %d, %Y');
-	return formatTime(new Date(date));
-}
-
-function dateFunctionNoYear(date) {
-	var formatTime = d3.timeFormat('%B %d');
-	return formatTime(new Date(date));
-}
-
-function increaseOrDecrease(value) {
-	if (value > 0) {
-		return 'increase';
-	} else {
-		return 'decrease';
-	}
-}
-
-// var radius = d3.scaleSqrt().domain([ -50, 50 ]).range([ 0, 25 ]);
-
-// label positions
-labely = height - 50;
-labelx = width - 280;
-
-// Add the year label; the value is set on transition.
-var label = svg
-	.append('text')
-	.attr('class', 'year label')
-	.attr('text-anchor', 'middle')
-	// position the label
-	.attr('y', labely)
-	.attr('x', labelx)
-	.text(dateFunction('2020-02-16'));
-
-var helperlabel = svg
-	.append('text')
-	.attr('class', 'helper label')
-	.attr('text-anchor', 'middle')
-	// position the label
-	.attr('y', labely + 20)
-	.attr('x', labelx)
-	.text('Hover to change date');
+tooltip = d3.select('body').append('div').attr('class', 'tooltip').style('opacity', 0);
 
 queue()
-	.defer(d3.json, 'https://d3js.org/us-10m.v1.json')
 	.defer(
 		d3.csv,
 		'https://raw.githubusercontent.com/connorrothschild/covid-mobility/master/data/county-data-long-averages.csv'
 	)
+	.defer(d3.json, 'us.json')
 	.await(ready);
 
-function ready(error, us, data) {
-	if (error) throw error;
+var legendText = [ '', '-50%', '', '', '0%', '', '', '+50%' ];
+var legendColors = [ '#b2182b', '#d6604d', '#f4a582', '#fddbc7', '#d1e5f0', '#92c5de', '#4393c3', '#2166ac' ];
 
-	var rateById = {},
-		nameById = {};
+function dateFunction(date) {
+	var formatTime = d3.time.format('%B %d, %Y');
+	return formatTime(new Date(date));
+}
+
+function dateFunctionNoYear(date) {
+	var formatTime = d3.time.format('%B %d');
+	return formatTime(new Date(date));
+}
+
+function ready(error, data, us) {
+	var counties = topojson.feature(us, us.objects.counties);
 
 	data.forEach(function(d) {
-		d.value = +d.value;
-		d.fips = d.fips;
-		rateById[d.fips] = +d.value;
-		nameById[d.fips] = d.Region;
-		stateById = d.State;
+		d.seconds = +new Date(d.date);
 		d.date = dateFunction(d.date);
+		d.fips = +d.fips;
+		d.value = +d.value;
+		d.county = d.Region;
 	});
 
-	// data = data.filter(
-	// 	(d = (d) => {
-	// 		return d.Category == 'Transit stations';
-	// 	})
-	// );
+	console.log(data);
 
-	// data = data.filter(
-	// 	(d = (d) => {
-	// 		return d.date == '2020-03-29';
-	// 	})
-	// );
-
-	var color = d3.scaleSequential(d3.interpolateSpectral).domain([ -50, 50 ]);
-
-	// https://blockbuilder.org/curran/3094b37e63b918bab0a06787e161607b
-	// Add a legend for the color values.
-	var legend = svg
-		.selectAll('.legend')
-		.data(color.ticks(8))
-		.enter()
-		.append('g')
-		.attr('class', 'legend')
-		.attr('transform', function(d, i) {
-			return 'translate(' + (width - 325 + i * 20) + ',' + 70 + ')';
-		});
-
-	legend.append('rect').attr('width', 20).attr('height', 20).style('fill', color);
-
-	legend
-		.append('text')
-		.attr('class', 'legend-labels')
-		.attr('x', 0)
-		.attr('y', 33) // .attr('dx', '.35em')
-		.text(function(d, i) {
-			if (i == 0 || i == 10 || i == 5) {
-				return d;
-			} else {
-				return null;
-			}
-		});
-
-	d3.select('.legend').append('text').attr('x', 0).attr('y', -12).attr('dx', '1.5em').text('% Change in mobility');
-
-	var box = label.node().getBBox();
-
-	var overlay = svg
-		.append('rect')
-		.attr('class', 'overlay')
-		.attr('x', box.x)
-		.attr('y', box.y)
-		.attr('width', box.width)
-		.attr('height', box.height)
-		.on('mouseover', enableInteraction);
-
-	var tool_tip = d3
-		.tip()
-		.attr('class', 'd3-tip')
-		// if the mouse position is greater than 650 (~ Kentucky/Missouri),
-		// offset tooltip to the left instead of the right
-		// credit https://stackoverflow.com/questions/28536367/in-d3-js-how-to-adjust-tooltip-up-and-down-based-on-the-screen-position
-		.offset(function() {
-			if (current_position[0] > 650) {
-				return [ -30, -240 ];
-			} else {
-				return [ 5, 30 ];
-			}
+	var dataByCountyByYear = d3
+		.nest()
+		.key(function(d) {
+			return d.fips;
 		})
-		.html("<div id='tipDiv'></div>");
+		.key(function(d) {
+			return d.seconds;
+		})
+		.map(data);
 
-	svg.call(tool_tip);
+	counties.features.forEach(function(county) {
+		county.properties.seconds = dataByCountyByYear[+county.id];
+	});
 
-	svg.transition().duration(5000).ease(d3.easeLinear).tween('date', tweenDate);
+	console.log(counties);
 
-	counties = svg
-		.append('g')
-		.attr('class', 'counties')
-		.selectAll('path')
-		.data(topojson.feature(us, us.objects.counties).features)
+	// var color = d3.scaleSequential(d3.interpolateSpectral).domain([ -50, 50 ]);
+	var color = d3.scale
+		.threshold()
+		.domain([ -50, -25, -10, 0, 10, 25, 50 ])
+		.range([ '#b2182b', '#d6604d', '#f4a582', '#fddbc7', '#d1e5f0', '#92c5de', '#4393c3', '#2166ac' ]);
+
+	var projection = d3.geo.albersUsa().translate([ width / 2, height / 2 ]);
+
+	var path = d3.geo.path().projection(projection);
+
+	var countyShapes = svg
+		.selectAll('.county')
+		.data(counties.features)
 		.enter()
 		.append('path')
+		.attr('class', 'county')
 		.attr('d', path)
-		.attr('stroke', 'grey')
-		.attr('stroke-width', 0.1)
-		// appending svg inside of tooltip for year by year change.
-		// h/t https://bl.ocks.org/maelafifi/ee7fecf90bb5060d5f9a7551271f4397
-		// h/t https://stackoverflow.com/questions/43904643/add-chart-to-tooltip-in-d3
-		.on('mouseover', function(d) {
-			// define and store the mouse position. this is used to define
-			// tooltip offset, seen above.
-			current_position = d3.mouse(this);
-			// console.log(current_position[0]);
+		.style('fill', 'grey');
 
-			current_county = nameById[d.id];
-			current_state = stateById;
+	svg
+		.append('path')
+		.datum(
+			topojson.feature(us, us.objects.states, function(a, b) {
+				return a !== b;
+			})
+		)
+		.attr('class', 'states')
+		.attr('d', path);
 
-			tool_tip.show();
-			var tipSVG = d3.select('#tipDiv').append('svg').attr('width', 220).attr('height', 55);
+	var legend = svg.append('g').attr('id', 'legend');
 
-			// tipSVG
-			// 	.append('circle')
-			// 	.attr('fill', function() {
-			// 		return color(rateById[d.id]);
-			// 	})
-			// 	.attr('stroke', 'black')
-			// 	.attr('cx', 180)
-			// 	.attr('cy', 30)
-			// 	.attr('r', function() {
-			// 		return radius(rateById[d.id]);
-			// 	});
-
-			tipSVG
-				.append('text')
-				.text(function() {
-					if (current_county == undefined) {
-						return '';
-					} else if (isNaN(rateById[d.id])) {
-						return '';
-					} else {
-						return (
-							Math.round(rateById[d.id]) + '% ' + increaseOrDecrease(rateById[d.id]) + ' from baseline'
-						);
-					}
-				}) // .transition()
-				// .duration(1000)
-				.attr('x', 0)
-				.attr('y', 55);
-
-			tipSVG
-				.append('text')
-				.attr('class', 'county-name')
-				.text(function() {
-					if (current_county == undefined) {
-						return 'No available data';
-					} else {
-						return current_county;
-					}
-				})
-				// .transition()
-				// .duration(1000)
-				.attr('x', 0)
-				.attr('y', 18);
-
-			tipSVG
-				.append('text')
-				.attr('class', 'state-name')
-				.text(function() {
-					if (current_county == undefined) {
-						return '';
-					} else {
-						return current_state;
-					}
-				})
-				// .transition()
-				// .duration(1000)
-				.attr('x', 0)
-				.attr('y', 35);
-		})
-		.on('mouseout', tool_tip.hide);
-
-	// counties.style('fill', function(d) {
-	// 	if (rateById[d.id] !== null) {
-	// 		return color(rateById[d.id]);
-	// 	} else {
-	// 		return '#FFFFFF';
-	// 	}
-	// });
-
-	// // create nation
-	// svg
-	// 	.append('path')
-	// 	.datum(topojson.feature(us, us.objects.nation))
-	// 	.attr('class', 'land')
-	// 	.attr('d', path)
-	// 	.attr('fill', 'none')
-	// 	.attr('stroke', 'grey')
-	// 	.attr('stroke-width', 0.2);
-
-	// // create the actual state objects
-	// svg
-	// 	.append('path')
-	// 	.datum(topojson.mesh(us, us.objects.states, (a, b) => a !== b))
-	// 	.attr('fill', 'none')
-	// 	.attr('stroke', 'grey')
-	// 	.attr('stroke-width', 0.15)
-	// 	.attr('stroke-linejoin', 'round')
-	// 	.attr('d', path);
-
-	function style(counties, date) {
-		newdata = interpolateData(date);
-
-		var rateById = {};
-		var nameById = {};
-
-		newdata.forEach(function(d) {
-			d.value = +d.value;
-			d.fips = d.fips;
-			rateById[d.fips] = +d.value;
-			nameById[d.fips] = d.Region;
-			stateById = d.State;
+	var legenditem = legend
+		.selectAll('.legenditem')
+		.data(d3.range(8))
+		.enter()
+		.append('g')
+		.attr('class', 'legenditem')
+		.attr('transform', function(d, i) {
+			return 'translate(' + i * 31 + ',0)';
 		});
 
-		console.log(newdata);
+	legenditem
+		.append('rect')
+		.attr('x', width - 240)
+		.attr('y', -7)
+		.attr('width', 30)
+		.attr('height', 6)
+		.attr('class', 'rect')
+		.style('fill', function(d, i) {
+			return legendColors[i];
+		});
 
-		// add fill according to death rates, for each id (state)
-		counties.style('fill', function(d) {
-			if (!isNaN(rateById[d.id])) {
-				return color(rateById[d.id]);
-			} else {
-				return 'white';
+	legenditem.append('text').attr('x', width - 240).attr('y', -10).style('text-anchor', 'middle').text(function(d, i) {
+		return legendText[i];
+	});
+
+	function update(second) {
+		slider.property('value', second);
+		console.log(second);
+		d3.select('.date').text(dateFunctionNoYear(second));
+		console.log(dateFunctionNoYear(second));
+		countyShapes.style('fill', function(d) {
+			if (d.properties.seconds !== undefined && !isNaN(d.properties.seconds[second][0].value)) {
+				return color(d.properties.seconds[second][0].value);
 			}
 		});
 
-		// create nation
-		svg
-			.append('path')
-			.datum(topojson.feature(us, us.objects.nation))
-			.attr('class', 'land')
-			.attr('d', path)
-			.attr('fill', 'none')
-			.attr('stroke', 'grey')
-			.attr('stroke-width', 0.2);
-
-		// create the actual state objects
-		svg
-			.append('path')
-			.datum(topojson.mesh(us, us.objects.states, (a, b) => a !== b))
-			.attr('fill', 'none')
-			.attr('stroke', 'grey')
-			.attr('stroke-width', 0.15)
-			.attr('stroke-linejoin', 'round')
-			.attr('d', path);
+		countyShapes
+			.on('mouseover', function(d) {
+				if (d.properties.seconds !== undefined && !isNaN(d.properties.seconds[second][0].value)) {
+					tooltip.transition().duration(250).style('opacity', 1);
+					tooltip
+						.html(
+							'<p><strong>' +
+								d.properties.seconds[second][0].county +
+								'</strong></p>' +
+								'<tr><td>Change in mobility on ' +
+								dateFunctionNoYear(d.properties.seconds[second][0].seconds) +
+								': </td><td><b>' +
+								formatPercent(d.properties.seconds[second][0].value / 100) +
+								'</b></td></tr>'
+						)
+						.style('left', d3.event.pageX + 15 + 'px')
+						.style('top', d3.event.pageY - 28 + 'px');
+				}
+			})
+			.on('mouseout', function(d) {
+				tooltip.transition().duration(250).style('opacity', 0);
+			});
 	}
 
-	// after the transition finishes, mouseover to change  year.
-	function enableInteraction() {
-		var dateScale = d3
-			.scaleLinear()
-			.domain(
-				d3.extent(data, function(d) {
-					return new Date(d.date);
-				})
-			)
-			.range([ box.x + 10, box.x + box.width - 10 ])
-			.clamp(true);
-
-		// Cancel the current transition, if any.
-		svg.transition().duration(0);
-
-		overlay
-			.on('mouseover', mouseover)
-			.on('mouseout', mouseout)
-			.on('mousemove', mousemove)
-			.on('touchmove', mousemove);
-
-		function mouseover() {
-			label.classed('active', true);
-		}
-		function mouseout() {
-			label.classed('active', false);
-		}
-		function mousemove() {
-			displayDate(dateScale.invert(d3.mouse(this)[0]));
-		}
-	}
-
-	// Tweens the entire chart by first tweening the year, and then the data.
-	// For the interpolated data, the dots and label are redrawn.
-	function tweenDate() {
-		var date = d3.interpolate(new Date('2020-02-16'), new Date('2020-03-29'));
-		return function(t) {
-			displayDate(date(t));
-		};
-	}
-
-	// Updates the display to show the specified year.
-	function displayDate(date) {
-		currentDate = date;
-		counties.call(style, date);
-		label.text(dateFunctionNoYear(date));
-	}
-
-	// Interpolates the dataset for the given (fractional) year.
-	function interpolateData(date) {
-		return data.filter(function(row) {
-			return row['date'] == dateFunction(date);
-			// return new Date(row.date) === new Date(date);
+	var slider = d3
+		.select('.slider')
+		.append('input')
+		.attr('type', 'range')
+		.attr('min', +new Date('2020-02-16'))
+		.attr('max', +new Date('2020-03-29'))
+		.attr('step', 1000 * 60 * 60 * 24)
+		.on('input', function() {
+			var second = +this.value;
+			update(second);
 		});
-	}
+
+	update(+new Date('2020-02-16'));
 }
