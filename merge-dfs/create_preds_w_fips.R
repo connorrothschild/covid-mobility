4424fe7a92946ed1e4627993628fef54d2c72abb
Connor Rothschild <connorrothschild@gmail.com>
2020-04-21 17:40:14 -0500

    county names + predictions

---
+++ merge-dfs/create_preds_w_fips.R
@@ -0,0 +1,18 @@
+predictions <- readr::read_csv('../regression/predictions.csv')
+
+library(tidycensus)
+library(tidyverse)
+
+fips <- fips_codes
+
+fips <- fips %>% 
+  mutate(county = paste0(stringr::str_to_title(county), " County"),
+         State = stringr::str_to_title(state))
+
+predictions_w_fips <- 
+  left_join(fips, predictions, by = "fips") %>% 
+  select(fips, county, State, pred)
+
+predictions_w_fips
+
+write.csv(predictions_w_fips, '../viz/data/mobility/county/predictions_w_fips.csv')
