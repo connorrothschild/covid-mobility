7767ac4c2dd77cbfb72fa14c086a8095e1778311
Connor Rothschild <connorrothschild@gmail.com>
2020-04-21 18:44:31 -0500

    with/without stay at home data

---
+++ merge-dfs/with_without_stayathome.R
@@ -0,0 +1,22 @@
+library(tidyverse)
+
+with_stay_at_home <- readr::read_csv('../viz/data/mobility/county/county-data-long.csv')
+without_stay_at_home <- readr::read_csv('../viz/data/mobility/county/predictions_w_fips.csv')
+
+with_latest <- with_stay_at_home %>% 
+  filter(date == "2020-03-29")
+
+with_clean <- with_latest %>% 
+  select(admin1, admin2, fips, value)
+
+without_clean <- without_stay_at_home %>% 
+  select(-X1)
+
+with_clean <- with_clean %>% 
+  rename('State' = 'admin1',
+         'county' = 'admin2',
+         'pred' = 'value')
+
+with_without_final <- full_join(with_clean, without_clean)
+
+write.csv(with_without_final, '../viz/data/mobility/county/with_without_stayathome.csv')
